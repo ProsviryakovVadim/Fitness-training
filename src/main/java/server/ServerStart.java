acc64417ea8e71e7cee3049721c008bc57e8b277
Vadim <Vadim@vadim.local>
2016-02-26 11:52:39 +0300

    server jetty start

---
+++ src/main/java/server/ServerStart.java
@@ -0,0 +1,24 @@
+package server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+
+
+public class ServerStart {
+
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8081);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+
+    }
+}
---
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,9 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class AllRequestsServlet extends HttpServlet {
+    public void a(){
+        System.out.printf("Hello");
+    }
+}
